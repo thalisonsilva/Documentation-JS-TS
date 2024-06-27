cebe76f175d8f02d2e4159dc7e8652cd9c4591f7
ThalisonSilva <devthalison@gmail.com>
2024-06-27 13:26:04 -0300

    Add:javascript

--- JS - Docs/01-introduction/Aula01.js
+++ JS - Docs/01-introduction/Aula01.js
@@ -4,3 +4,4 @@ fruits[5] = "mango";
 console.log(fruits[5]); // 'mango'
 console.log(Object.keys(fruits)); // ['0', '1', '2', '5']
 console.log(fruits.length); // 6
+console.log(fruits.length); // 7
